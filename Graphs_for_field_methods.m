f3228de5aba5d6e4bf31b29de4fbda3f93da2ba4
Drenne68 <drenne68@students.rowan.edu>
2024-11-22 13:00:49 -0500

    Big Update
    Colin still hasn't do anything

--- Graphs_for_field_methods.m
+++ Graphs_for_field_methods.m
@@ -1,7 +1,12 @@
 %% Making graphs for field methods
 %%
 %First we're adding a path to the data we're using
-addpath C:\Users\slend\Documents\MATLAB\Data
+addpath C:\Users\slend\Documents\GitHub\Field-methods-\Data\
+%Dan's
+addpath C:\Users\Hocke\OneDrive\Documents\GitHub\Field-methods-\Data\
+%Hocke's
+addpath C:\Users\wyatt\Documents\GitHub\Field-methods-\Data\
+%Watts
 filename = 'Field Methods Data Fall 2024.xlsx'
 Resperation_Data = readtable('Field Methods Data Fall 2024.xlsx','Sheet','Soil Respiration')
 %%
