21545814570c5e4f61a86a514e5b696d2e1eff8a
Drenne68 <drenne68@students.rowan.edu>
2024-11-22 12:50:31 -0500

    OMG IM GONNA KILL COLIN

---
+++ Graphs_for_field_methods.m
@@ -0,0 +1,7 @@
+%% Making graphs for field methods
+%%
+%First we're adding a path to the data we're using
+addpath C:\Users\slend\Documents\MATLAB\Data
+filename = 'Field Methods Data Fall 2024.xlsx'
+Resperation_Data = readtable('Field Methods Data Fall 2024.xlsx','Sheet','Soil Respiration')
+%%
